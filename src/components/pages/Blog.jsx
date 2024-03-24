2346c25b7d57623b7e1e2a120a5be25c01bf0c09
Nkemakolauko <nwekevino99@gmail.com>
2024-03-24 01:39:57 +0000

    Make the articles have an infinite scroll --

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -7,17 +7,14 @@ import { ToastContainer, toast } from "react-toastify";
 import BlogsCard from "./components/BlogsCard";
 import { useContext } from "react";
 import { ThemeContext } from "../../App";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Blog = () => {
   const { theme, setTheme } = useContext(ThemeContext);
   const [iframeLoaded, setIframeLoaded] = useState(true);
   const [hamburger, setHamburger] = useState(false);
   const [articles, setArticles] = useState([]);
-
-  // useEffect(() => {
-  //   setIframeLoaded(false);
-  //   setTimeout(() => setIframeLoaded(true), 5000);
-  // }, []);
+  const [page, setPage] = useState(1);
 
   const handleOpenHamburger = () => {
     setHamburger(true);
@@ -26,26 +23,6 @@ const Blog = () => {
   const handleHideHamburger = () => {
     setHamburger(false);
   };
-
-  // useEffect(() => {
-  //   const getNews = async () => {
-  //     try {
-  //       let response = await fetch(
-  //         "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=bde4336c93c8418e921eea9818e9633a"
-  //       );
-
-  //       let jsonData = await response.json();
-  //       console.log(jsonData.articles);
-  //       setArticles(jsonData.articles);
-  //       setIframeLoaded(false);
-  //     } catch (error) {
-  //       console.log(error);
-  //       setIframeLoaded(true);
-  //     }
-  //   };
-
-  //   getNews();
-  // }, []);
 
   useEffect(() => {
     fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
@@ -65,9 +42,40 @@ const Blog = () => {
       .catch((error) => {
         setIframeLoaded(true);
         console.log(error);
-        setIframeLoaded(false);
+        // setIframeLoaded(false);
       });
   }, []);
+
+  // Fetch more news
+  const fetchMoreData = () => {
+    setPage(page + 1);
+    fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?page=${page}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const promises = data
+          .slice(0, 10)
+          .map((id) =>
+            fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+              (res) => res.json()
+            )
+          );
+        Promise.all(promises)
+          .then((newArticles) => {
+            setArticles([...articles, ...newArticles]);
+            setIframeLoaded(false);
+          })
+          .catch((error) => {
+            setIframeLoaded(true);
+            console.log(error);
+            // setIframeLoaded(false);
+          });
+      })
+      .catch((error) => {
+        setIframeLoaded(true);
+        console.log(error);
+        setIframeLoaded(false);
+      });
+  };
 
   return (
     <div className="dark" data-theme={theme}>
@@ -75,7 +83,7 @@ const Blog = () => {
       {hamburger && <HeaderHamburger hideHamburger={handleHideHamburger} />}
       <ToastContainer position="top-left" />
       <PageTitle title="Articles" />
-      <div className="max-w-3xl mx-auto pb-28 flex flex-col items-center gap-10">
+      <div className="max-w-4xl mx-auto pb-28 flex flex-col items-center gap-10">
         <div className="border hover:border-black hover:text-black hover:text-[14px] hover:bg-white border-t-0 h-[40px] w-[350px] text-center justify-center flex items-center text-white bg-blue-500 border-blue-500 transition-all duration-500 font-semibold truncate">
           PERSONAL AND HOSTED - IN PROGRESS
         </div>
@@ -86,29 +94,32 @@ const Blog = () => {
               Getting Articles...
             </div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 mx-4 w-[95%] md:w-[100%]">
-              {articles.map((article, i) => {
-                return (
-                  <BlogsCard
-                    url={article.url}
-                    mainSite={article.url}
-                    key={i}
-                    time={article.time}
-                    title={article.title}
-                    by={article.by}
-                  />
-                  // <BlogsCard
-                  //   url={article.url}
-                  //   mainSite={article.url}
-                  //   key={i}
-                  //   time={article.publishedAt}
-                  //   title={article.title}
-                  //   by={article.author}
-                  //   img={article.urlToImage}
-                  // />
-                );
-              })}
-            </div>
+            <InfiniteScroll
+              dataLength={articles.length}
+              next={fetchMoreData}
+              hasMore={true}
+              style={{ overflow: "hidden", width: "100%" }}
+              loader={
+                <h3 style={{ textAlign: "center", marginTop: "20px" }}>
+                  Loading...
+                </h3>
+              }
+            >
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 mx-4 w-[95%] md:w-[100%]">
+                {articles.map((article, i) => {
+                  return (
+                    <BlogsCard
+                      url={article.url}
+                      mainSite={article.url}
+                      key={i}
+                      time={article.time}
+                      title={article.title}
+                      by={article.by}
+                    />
+                  );
+                })}
+              </div>
+            </InfiniteScroll>
           )}
         </div>
       </div>
--- src/components/pages/components/BlogsCard.jsx
+++ src/components/pages/components/BlogsCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CiClock2 } from "react-icons/ci";
 import img from "../../../assets/v-v-v.webp";
+import newImg from "../../../assets/articles.jpg";
 
 const BlogsCard = ({
   toArticle,
@@ -13,7 +14,7 @@ const BlogsCard = ({
   // img,
 }) => {
   return (
-    <div className="w-full h-[100%] border flex items-center justify-center rounded-b-[4px] rounded-t-[15px] bg-slate-100 flex-col text-slate-600 shadow-lg hover:shadow-black/50 transition-all duration-300">
+    <div className="max-w-[350px] w-full h-[100%] border flex items-center justify-center rounded-b-[4px] rounded-t-[15px] bg-slate-100 flex-col text-slate-600 shadow-lg hover:shadow-black/50 transition-all duration-300">
       <p className="mr-auto pl-2 my-1 flex items-center gap-1 italic text-xs">
         <span>Posted :</span>
         <span className=""> {time}</span>
@@ -23,11 +24,11 @@ const BlogsCard = ({
         target="_blank"
         rel="noopener noreferrer"
         title="Click to visit"
-        className="w-[95%] h-[60%] border rounded-md bg-white flex flex-col hover:scale-95 active:scale-95 transition-all duration-300"
+        className="max-w-[350px] w-[95%] h-[60%] border rounded-md bg-white flex flex-col hover:scale-95 active:scale-95 transition-all duration-300"
       >
         <div className="w-full h-[70%] rounded-t-md">
           <img
-            src={img}
+            src={newImg}
             alt="Blog Image"
             className="rounded-t-md h-full w-full"
           />
@@ -40,7 +41,7 @@ const BlogsCard = ({
           <p className="text-[10px] text-slate-500 truncate">{mainSite}</p>
         </div>
       </a>
-      <div className="w-full px-3 h-[40%] flex flex-col justify-center mr-auto">
+      <div className="max-w-[350px] w-full px-3 h-[40%] flex flex-col justify-center mr-auto">
         <p className="text-slate-900 text-sm">
           {title} - {by}
         </p>
@@ -54,54 +55,6 @@ const BlogsCard = ({
         </a>
       </div>
     </div>
-    // <div className="w-full h-[100%] border flex items-center justify-center rounded-b-[4px] rounded-t-[15px] bg-slate-100 flex-col text-slate-600 shadow-lg hover:shadow-black/50 transition-all duration-300">
-    //   <p className="mr-auto pl-2 my-1 flex items-center gap-1 italic text-xs">
-    //     <span>Posted :</span>
-    //     <span className=""> 14 - 02 - 2024</span>
-    //   </p>
-    //   <a
-    //     href={toArticle}
-    //     target="_blank"
-    //     rel="noopener noreferrer"
-    //     title="Click to visit"
-    //     className="w-[95%] h-[60%] border rounded-md bg-white flex flex-col hover:scale-95 active:scale-95 transition-all duration-300"
-    //   >
-    //     <div className="w-full h-[60%] mb-2 rounded-t-md">
-    //       <img
-    //         src={img}
-    //         alt="Blog Image"
-    //         className="rounded-t-md h-full w-full"
-    //       />
-    //     </div>
-    //     <div className="w-full h-[40%] px-2 bg-slate-200 rounded-b-md flex flex-col justify-center">
-    //       <p className="text-sm text-black">
-    //         How To Create A Login System Using Firebase And ReactJS - Traversy
-    //         Media
-    //       </p>
-    //       <p className="text-slate-800 text-xs truncate">
-    //         Apparently what I will be displaying here will be a little
-    //         description of what was posted, yunno - for curiosity.
-    //       </p>
-    //       <p className="text-[10px] text-slate-500 truncate">
-    //         https://thewebsite.com/
-    //       </p>
-    //     </div>
-    //   </a>
-    //   <div className="w-full px-3 h-[40%] flex flex-col justify-center">
-    //     <p className="text-slate-900 text-sm">
-    //       How To Create A Login System Using Firebase And ReactJS - Traversy
-    //       Media
-    //     </p>
-    //     <a
-    //       href="https://thewebsite.com/the-article-itself/what-i-want-to-show/"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //       className="text-blue-500 hover:text-blue-700 text-sm"
-    //     >
-    //       https://thewebsite.com/the-article-itself/what-i-want-to-show/
-    //     </a>
-    //   </div>
-    // </div>
   );
 };
 
