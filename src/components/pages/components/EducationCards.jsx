47b9818a2c543e233c2cf608cd4edce00cd7f9f0
Nkemakolauko <nwekevino99@gmail.com>
2024-06-09 07:15:41 +0100

    Fix the UI issue in Education page -

--- src/components/pages/components/EducationCards.jsx
+++ src/components/pages/components/EducationCards.jsx
@@ -6,13 +6,13 @@ import { FaArrowRight } from "react-icons/fa6";
 const EducationCards = ({ startYear, endYear, type, school, cert }) => {
   return (
     <>
-      <div className="grid grid-cols-3 justify-between gap-4 mb-4 mt-4 border border-dashed divide-x divide-dashed hover:shadow-md cursor-default transition-all duration-300">
-        <div className="col-span-1 py-4 md:p-4">
+      <div className="grid grid-cols-3 justify-between md:gap-4 mb-4 mt-4 border border-dashed divide-x divide-dashed hover:shadow-md cursor-default transition-all duration-300">
+        <div className="col-span-1 py-4 md:p-4 px-1">
           <div className="flex items-center gap-0">
             <div className="w-[100%] md:w-[100%] lg:w-[100%] text-xs md:text-sm flex items-center">
-              <span className="px-2">{startYear}</span>{" "}
+              <span className="px-1">{startYear}</span>{" "}
               {endYear && <FaArrowRight />}{" "}
-              <span className="px-2">{endYear}</span>
+              <span className="px-1">{endYear}</span>
             </div>
           </div>
         </div>
